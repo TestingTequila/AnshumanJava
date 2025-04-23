fa6d1b16872fc59a00e04ca563a1347a8d087ec2
Ashish Mishra <ashish.mishra.sse@gmail.com>
2025-04-23 18:33:30 +0530

    methods

---
+++ src/com/janbask/methods/MethodSignature.java
@@ -0,0 +1,28 @@
+package com.janbask.methods;
+
+public class MethodSignature {
+
+    int x;
+    int y;
+
+    public void addition(int x, int y) {
+        int sum = x + y;
+        System.out.println("Addition:" + sum);
+    }
+
+    public void subtraction(int x, int y) {
+        int diff = x - y;
+        System.out.println("Subtraction:" + diff);
+    }
+
+    public  void multiplication()
+    {
+        int x=5;
+        int y=4;
+         double tax = 0.3;
+        int product =x*y;
+        System.out.println("Multiplication: "+  product);
+    }
+
+
+}
---
+++ src/com/janbask/methods/TestMethods.java
@@ -0,0 +1,13 @@
+package com.janbask.methods;
+
+public class TestMethods
+{
+    public static void main(String[] args) {
+        MethodSignature ms = new MethodSignature();
+
+        ms.addition(12,8);
+        ms.subtraction(22,7);
+        ms.multiplication();
+
+    }
+}
